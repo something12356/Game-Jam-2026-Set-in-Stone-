5e55e2c34f3b76890e825ce9eeb95495f486fa4a
Marcell Perger <marcell.perger.1@gmail.com>
2026-03-01 12:50:27 +0000

    Buttons work, larger scrrenn, wonrg order

--- ui.py
+++ ui.py
@@ -11,7 +11,7 @@ from pygame import FRect, Rect as IRect
 import factoryMechanics as backend
 from factoryMechanics import (
     Factory, CopperMineBasic, CopperMineAdvanced, IronMine, Copper, Iron,
-    Building, Contract, NullResource)
+    Building, Contract, NullResource, Yooperlite, DragonEgg, Elbaite)
 
 ORE_TEXT_COLOR = 'white'
 BUILDING_TEXT_COLOR = 'white'
@@ -28,13 +28,13 @@ class ScreenInfo:
         self.menu_area = self.rem_area.scale_by(1, 0.1).move_to(topleft=self.main_area.bottomleft)
         self.base_player_area = self.main_area.scale_by(0.5, 0.5).move_to(topleft=self.main_area.topleft)
         self.base_player_area_rel = self.base_player_area.move_to(topleft=(0, 0))
-        self.player_left_area = self.base_player_area.scale_by(0.2, 1).move_to(
+        self.player_left_area = self.base_player_area.scale_by(0.22, 1).move_to(
             topleft=(0, 0))
-        self.player_ores_area = self.player_left_area.scale_by(1, 0.6).move_to(
+        self.player_ores_area = self.player_left_area.scale_by(1, 0.55).move_to(
             topleft=self.player_left_area.topleft)
-        self.player_buttons_area = self.player_left_area.scale_by(1, 0.4).move_to(
+        self.player_buttons_area = self.player_left_area.scale_by(1, 0.45).move_to(
             topleft=self.player_ores_area.bottomleft)
-        self.player_right_area = self.base_player_area.scale_by(0.8, 1).move_to(
+        self.player_right_area = self.base_player_area.scale_by(0.78, 1).move_to(
             topleft=self.player_left_area.topright)
         self.player_buildings_area = self.player_right_area.scale_by(1, 0.4).move_to(
             topleft=self.player_right_area.topleft)
@@ -52,7 +52,7 @@ class ScreenInfo:
         return self
 
 
-SC_INFO = ScreenInfo().from_sc_size(Vec2(1200, 750))
+SC_INFO = ScreenInfo().from_sc_size(Vec2(1300, 900))
 
 
 def clamped_subsurf(s: pygame.Surface, r: IRect | FRect):
@@ -172,18 +172,47 @@ class Player:
             width=w-10, height=tex.height + 6, center=txr.center))
         dest.blit(tex, txr)
         y = txx.bottom + 5
-        self.buttons += [(txr, self.petrify_action)]
+        self.buttons += [(txx, self.petrify_action)]
 
         tex = load_from_fontspec('Helvetica', 'sans-serif').render(
-            'Add fire opal', True, 'white'
+            'Add FireOpal', True, 'white'
         )
         txr = tex.get_rect(top=y, centerx=SC_INFO.player_buttons_area.centerx)
         txx = pygame.draw.rect(dest, Color(50, 50, 50), txr.move_to(
             width=w - 10, height=tex.height + 6, center=txr.center))
         dest.blit(tex, txr)
         y = txx.bottom + 5
+        self.buttons += [(txx, lambda: self.factory.add_ore("FireOpal", 1))]
 
-        self.buttons += [(txr, lambda: self.factory.add_ore("FireOpal", 1))]
+        tex = load_from_fontspec('Helvetica', 'sans-serif').render(
+            'Add Yooperlite', True, 'white'
+        )
+        txr = tex.get_rect(top=y, centerx=SC_INFO.player_buttons_area.centerx)
+        txx = pygame.draw.rect(dest, Color(50, 50, 50), txr.move_to(
+            width=w - 10, height=tex.height + 6, center=txr.center))
+        dest.blit(tex, txr)
+        y = txx.bottom + 5
+        self.buttons += [(txx, lambda: self.factory.add_ore(Yooperlite.name, 1))]
+
+        tex = load_from_fontspec('Helvetica', 'sans-serif').render(
+            'Add DragonEgg', True, 'white'
+        )
+        txr = tex.get_rect(top=y, centerx=SC_INFO.player_buttons_area.centerx)
+        txx = pygame.draw.rect(dest, Color(50, 50, 50), txr.move_to(
+            width=w - 10, height=tex.height + 6, center=txr.center))
+        dest.blit(tex, txr)
+        y = txx.bottom + 5
+        self.buttons += [(txx, lambda: self.factory.add_ore(DragonEgg.name, 1))]
+
+        tex = load_from_fontspec('Helvetica', 'sans-serif').render(
+            'Add Elbaite', True, 'white'
+        )
+        txr = tex.get_rect(top=y, centerx=SC_INFO.player_buttons_area.centerx)
+        txx = pygame.draw.rect(dest, Color(50, 50, 50), txr.move_to(
+            width=w - 10, height=tex.height + 6, center=txr.center))
+        dest.blit(tex, txr)
+        y = txx.bottom + 5
+        self.buttons += [(txx, lambda: self.factory.add_ore(Elbaite.name, 1))]
 
     def petrify_action(self):
         self.factory.blockedFromPlaying = max(self.factory.blockedFromPlaying, 2)
