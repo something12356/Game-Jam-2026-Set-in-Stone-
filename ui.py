5d10f9f2bbea499cc5a14fd204856a1b64dd3c30
Marcell Perger <marcell.perger.1@gmail.com>
2026-03-01 12:44:29 +0000

    Add fire opals

--- ui.py
+++ ui.py
@@ -28,14 +28,13 @@ class ScreenInfo:
         self.menu_area = self.rem_area.scale_by(1, 0.1).move_to(topleft=self.main_area.bottomleft)
         self.base_player_area = self.main_area.scale_by(0.5, 0.5).move_to(topleft=self.main_area.topleft)
         self.base_player_area_rel = self.base_player_area.move_to(topleft=(0, 0))
-        self.player_left_area = self.base_player_area.scale_by(0.18, 1).move_to(
+        self.player_left_area = self.base_player_area.scale_by(0.2, 1).move_to(
             topleft=(0, 0))
-        self.player_ores_area = self.player_left_area.scale_by(1, 0.5).move_to(
+        self.player_ores_area = self.player_left_area.scale_by(1, 0.6).move_to(
             topleft=self.player_left_area.topleft)
-        self.player_buttons_area = self.player_left_area.scale_by(1, 0.5).move_to(
-            topleft=self.player_ores_area.bottomleft
-        )
-        self.player_right_area = self.base_player_area.scale_by(0.82, 1).move_to(
+        self.player_buttons_area = self.player_left_area.scale_by(1, 0.4).move_to(
+            topleft=self.player_ores_area.bottomleft)
+        self.player_right_area = self.base_player_area.scale_by(0.8, 1).move_to(
             topleft=self.player_left_area.topright)
         self.player_buildings_area = self.player_right_area.scale_by(1, 0.4).move_to(
             topleft=self.player_right_area.topleft)
@@ -164,15 +163,27 @@ class Player:
     def render_side_buttons(self, dest: pygame.Surface):
         y = SC_INFO.player_buttons_area.top + 5
         # x = SC_INFO.player_buttons_area.left + 5
-        w = SC_INFO.player_buttons_area.width - 10
+        w = SC_INFO.player_buttons_area.width
         tex = load_from_fontspec('Helvetica', 'sans-serif').render(
             'Petrify', True, 'white'
         )
         txr = tex.get_rect(top=y, centerx=SC_INFO.player_buttons_area.centerx)
-        pygame.draw.rect(dest, Color(50, 50, 50), txr.move_to(
+        txx = pygame.draw.rect(dest, Color(50, 50, 50), txr.move_to(
             width=w-10, height=tex.height + 6, center=txr.center))
         dest.blit(tex, txr)
+        y = txx.bottom + 5
         self.buttons += [(txr, self.petrify_action)]
+
+        tex = load_from_fontspec('Helvetica', 'sans-serif').render(
+            'Add fire opal', True, 'white'
+        )
+        txr = tex.get_rect(top=y, centerx=SC_INFO.player_buttons_area.centerx)
+        txx = pygame.draw.rect(dest, Color(50, 50, 50), txr.move_to(
+            width=w - 10, height=tex.height + 6, center=txr.center))
+        dest.blit(tex, txr)
+        y = txx.bottom + 5
+
+        self.buttons += [(txr, lambda: self.factory.add_ore("FireOpal", 1))]
 
     def petrify_action(self):
         self.factory.blockedFromPlaying = max(self.factory.blockedFromPlaying, 2)
